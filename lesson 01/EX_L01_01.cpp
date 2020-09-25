169cfb45a9c42efc7af8a8820143086869fe50d2
Seng Sourng <sengsourng@gmail.com>
2020-09-25 12:36:54 +0700

    update

---
+++ lesson 01/EX_L01_01.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	//Display welcome application
+	cout<<"Welcome to first Application in C++"<<endl;	
+	cout<<"Hello, Dara \n";
+	
+	return(0);
+}
+
